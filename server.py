8be7b8269a0837da9380ac5860d89d962664b1f5
gesifeta <adamgemechu@gmail.com>
2025-07-20 14:09:40 +0300

    added dynamic year to the footer

--- server.py
+++ server.py
@@ -1,17 +1,18 @@
 from flask import Flask, render_template
 import json
+import datetime
 app = Flask(__name__)
-
+year = datetime.date.today().year
 with open("blog.json") as f:
     posts = json.load(f)
 @app.route('/')
 def index():
-    return render_template('index.html',posts=posts)
+    return render_template('index.html',posts=posts, year=year)
 
 @app.route('/post/<int:id>')
 def get_post(id):
     post = posts[id]
-    return render_template("index.html",posts=[post],id=id, len = len(post))
+    return render_template("index.html",posts=[post],id=id, len = len(post),year=year)
 
 
 if __name__ == '__main__':
