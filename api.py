6f45762a0b39619e21aca4f17fafa0c7150ac4a0
gesifeta <adamgemechu@gmail.com>
2025-07-21 14:00:00 +0300

    added contact for

---
+++ api.py
@@ -0,0 +1,4 @@
+import os
+
+GMAIL_USERNAME = 'adamgemechu@gmail.com'
+GMAIL_PASSWORD = 'msqy tgqh wtmt suuf'
--- server.py
+++ server.py
@@ -1,8 +1,13 @@
-from flask import Flask, render_template
+from sys import exception
+from flask import Flask, render_template, request
+from  api import GMAIL_PASSWORD,GMAIL_USERNAME
+
+import smtplib,os
 import json
 import datetime
 app = Flask(__name__)
 year = datetime.date.today().year
+
 with open("blog.json") as f:
     posts = json.load(f)
 @app.route('/')
@@ -17,15 +22,34 @@ def get_post(id):
 
 @app.route("/about")
 def about():
-    return "About"
+    return render_template("about.html")
 
-@app.route("/contact")
+@app.route("/contact",methods=["GET","POST"])
 def contact():
-    return "Contact"
+    return render_template("contact.html")
+
+
+@app.route("/login",methods=["POST","GET"])
+def login():
+    if request.method == "POST":
+        if request.form["email"] != "":
+            try:
+                with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
+                    connection.starttls()
+                    connection.login(user=GMAIL_USERNAME,password= GMAIL_PASSWORD)
+            except Exception as e:
+                    return f"Login Failed: {e}"
+            else:
+                connection.sendmail(from_addr=GMAIL_USERNAME, to_addrs="adamgemechu@outlook.com",
+                    msg=f"Subject:Blog Post\n\n{request.form['message']}")
+                connection.quit()
+
+    else:
+        return render_template("contact.html")
 
 @app.route("/posts")
 def sample_blog_posts():
-    return "Sample Posts"
+    return render_template("post.html")
 
 
 if __name__ == '__main__':
